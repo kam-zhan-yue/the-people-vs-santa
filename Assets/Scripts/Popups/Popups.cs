f8aac3a2552cbea3a6d4230bcd9f67844ec2444a
kamzhanyue <kamzhanyue@gmail.com>
2025-07-06 22:58:48 +1000

    add shaking

--- Assets/Scripts/Popups/Popups.cs
+++ Assets/Scripts/Popups/Popups.cs
@@ -1,7 +1,15 @@
+using DG.Tweening;
+using Kuroneko.AudioDelivery;
+using Kuroneko.UIDelivery;
+using Kuroneko.UtilityDelivery;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
-public class Popups : MonoBehaviour
+public class Popups : Popup
 {
+    [SerializeField] private float shakeDuration = 0.2f;
+    [SerializeField] private float shakeStrength = 5f;
+    [SerializeField] private int shakeVibrato = 20;
     [SerializeField] private SpeakerPopup speakerPopup;
     [SerializeField] private DialoguePopup dialoguePopup;
     [SerializeField] private ActionPopup actionPopup;
@@ -11,6 +19,13 @@ public class Popups : MonoBehaviour
     public SpeakerPopup SpeakerPopup => speakerPopup;
     public ActionPopup ActionPopup => actionPopup;
     public GameOverPopup GameOverPopup => gameOverPopup;
+    
+    private Vector2 _originalPosition;
+
+    protected override void InitPopup()
+    {
+        _originalPosition = mainHolder.anchoredPosition;
+    }
 
     public void Init(Game game)
     {
@@ -26,5 +41,31 @@ public class Popups : MonoBehaviour
         dialoguePopup.HidePopup();
         actionPopup.HidePopup();
         gameOverPopup.ShowPopup();
+    }
+
+    [Button]
+    public void Shake()
+    {
+        // Shake Tween
+        mainHolder.DOKill();
+        mainHolder.DOShakePosition(
+            duration: shakeDuration,
+            strength: new Vector3(shakeStrength, shakeStrength, 0f),
+            vibrato: shakeVibrato,
+            randomness: 90f,
+            snapping: false,
+            fadeOut: true
+        )
+        .OnKill(() =>
+        {
+            mainHolder.anchoredPosition = _originalPosition;
+        })
+        .OnComplete(() =>
+        {
+            mainHolder.anchoredPosition = _originalPosition;
+        });;
+        
+        // Shake Audio
+        ServiceLocator.Instance.Get<IAudioService>().Play("SHAKE");
     }
 }
